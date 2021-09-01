d38201cbed77343c554cf3e9ee9df4cc7e9ea432
Prethush <ktmirash49@.gmail.com>
2021-09-01 14:42:31 +0530

    refactor

--- blog-app/src/components/Home.js
+++ blog-app/src/components/Home.js
@@ -5,7 +5,7 @@ function Home() {
         <main>
             <div className="text-center pt-12">
                 <h2 className="text-5xl font-bold mb-8">Welcome to Blog App</h2>
-                < Link exact to="/articles">
+                < Link to="/articles">
                     <button className="bg-blue-500 text-white font-bold px-4 py-2 rounded-md">Go to Articles Page</button>
                 </Link>
             </div>
--- blog-app/src/components/Main.js
+++ blog-app/src/components/Main.js
@@ -1,90 +1,60 @@
 import React from "react";
 import Articles from "./Articles";
 import Tags from "./Tags";
+import {articlesURL}  from "../utils/constant";
+import Pagination from "./Pagination";
 
 class Main extends React.Component {
-    constructor(props){
-        super();
-        this.state = {
-            offset: 0,
-            articles: null ,
-            tags: null,
-            allData: null,
-            tagSelected: null
-        };
-    }
+   
+        constructor(props) {
+            super();
 
-    getAllTags = () => {
-        console.log("hello");
-        fetch(`https://mighty-oasis-08080.herokuapp.com/api/articles?limit=all`)
-        .then((res) => res.json())
-        .then((data) => {
-           let allTags = data.articles.map(article => article.tagList).flat(Infinity);
-            let arr =  allTags.reduce((acc, curr) => {
-                if(curr){
-                    if(!acc.includes(curr)) {
-                        acc.push(curr.trim());
-                    }
-                } 
-                return acc;
-           }, []);
-           
-            this.setState({tags: arr});
-        })
-    }
-
-    getUrl = () => {
-        if(this.state.tagSelected) {
-            return `https://mighty-oasis-08080.herokuapp.com/api/articles?limit=all&tag=${this.state.tagSelected}`
-        }else {
-            return `https://mighty-oasis-08080.herokuapp.com/api/articles?limit=all`
+            this.state = {
+                articles: null,
+                error: "",
+                articlesCount: null,
+                articlesPerPage: 10,
+                activePage: 1,
+                tagSelected: ""
+            }
         }
-    }
-    getArticles = () => {
-        let {offset} = this.state;
-        fetch(this.getUrl())
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
-            this.setState({articles: data.articles.slice(offset, offset + 10), allData: data.articles});
-        });
-    }
+    
 
     componentDidMount() {
-        this.getArticles();
-        this.getAllTags();
+       this.getArticles();
     }
 
-    getTotalPages = () => {
-        let {allData} = this.state;
-        let numberOfPages = Math.floor(allData.length/10);
-        let arr = [];
-        for(let i = 0; i < numberOfPages; i++){
-            arr.push(i+1);
-        }
-        return arr;
-    }
-
+   
     handleClick = ({target}) => {
         let {id} = target.dataset;
-        this.setState((prevState) => {
-            return {
-                offset: id * 10
+        this.setState({activePage: id}, this.getArticles);
+    }
+
+    getArticles = () => {
+        let offset = (this.state.activePage - 1) * 10;
+        let tag = this.state.tagSelected;
+        fetch(articlesURL + `/?limit=${this.state.articlesPerPage}&offset=${offset}` + (tag && `&tag=${tag}`))
+        .then((res) => {
+            if(!res.ok)  {
+                throw new Error(res.statusText);
             }
-        }, () => this.getArticles());
+            return res.json();
+        })
+        .then((data) => {
+            console.log(data);
+            this.setState({articles: data.articles, articlesCount: data.articlesCount});
+        })
+        .catch((err) => {
+            this.setState({error: "Not able to fetch Articles"});
+        });
     }
 
     selectTag = ({target}) => {
         let {value} = target.dataset;
-        this.setState((prevState) => {
-            return {
-                tagSelected: value,
-                offset: 0
-            }
-        }, () => this.getArticles());
+        this.setState({tagSelected: value}, this.getArticles);
     }
     render() {
-        
+        let {articles, error, articlesCount, articlesPerPage, activePage} = this.state;
          
         return (
             
@@ -93,12 +63,9 @@ class Main extends React.Component {
 
                     {/* feeds part */}
                         <div className="flex mb-3">
-                            <span className="cursor-pointer text-xl text-blue-900" onClick={() => this.setState((prevState) => {
-                                return {
-                                    tagSelected: null,
-                                   
-                                }
-                            }, () => this.getArticles())}>Global Feed</span>
+                            <span className="cursor-pointer text-xl text-blue-900" onClick={() => this.setState({
+                                tagSelected: ""
+                            }, this.getArticles)}>Global Feed </span>
                             <div className={this.state.tagSelected ? "visible text-xl": "hidden"}>
                                 <span className="mx-2 text-gray-500">/</span>
                                 <span className="text-green-700">#{this.state.tagSelected}</span>
@@ -108,26 +75,23 @@ class Main extends React.Component {
                         {/* articles part */}
                         <section className="flex justify-between ">
                             <div className="flex-70">
-                                {
-                                    !this.state.articles ? <h2>Loading...</h2> : < Articles {...this.state} />
-                                }
+                                < Articles articles={articles} error={error} />
                             </div>
 
                         {/* tags part */}
                             <div className="flex-25">
-                                {
-                                    !this.state.tags ? <h2>Loading...</h2> : < Tags {...this.state} selectTag = {this.selectTag} />
-                                }
+                                < Tags selectTag={this.selectTag}/>
                             </div>
                         </section>
                             
                         {/* page indicator */}
                         <div className="text-center py-8">
-                            {
-                                this.state.allData ? this.getTotalPages().map((p, i) => {
-                                   return <span key={p} className={this.state.offset === i * 10 ? "bg-black text-white py-2 px-4 mx-4 cursor-pointer": "border border-gray-300 py-2 px-4 mx-4 cursor-pointer"} data-id={i} onClick={(e) => this.handleClick(e)}>{p}</span>
-                                }) : ""
-                            }
+                            < Pagination 
+                            articlesCount={articlesCount} 
+                            articlesPerPage={articlesPerPage}
+                            activePage={activePage}
+                            handleClick = {this.handleClick}
+                            />
                         </div>
                 </main>
             
